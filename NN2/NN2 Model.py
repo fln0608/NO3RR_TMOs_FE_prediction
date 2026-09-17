04da1f7c68d5cf97d4d8daf8f221ac08f8e3cfc7
fln0608 <3394657692@qq.com>
2026-09-17 12:38:08 +0800

    Update NN2 Model.py

--- NN2/NN2 Model.py
+++ NN2/NN2 Model.py
@@ -18,12 +18,17 @@ import os
 
 # Increase the reproducibility of the model
 def set_deterministic(seed):
+
     torch.manual_seed(seed)
     np.random.seed(seed)
+
     if torch.cuda.is_available():
+
         torch.cuda.manual_seed_all(seed)
+
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
+
     random.seed(seed)
 set_deterministic(42)
 
